ac7ddfe79daf40c412de486e0eea5261208450c4
Dashel Bradshaw <Bradshawdc@s.dcsdk12.org>
2019-11-05 09:24:52 -0700

    introductory

---
+++ Java.js
@@ -0,0 +1,7 @@
+// alert("Hello, Code!");
+document.body.style.background = "#0f2fff";
+setTimeout(() => {
+    document.body.style.background = "grey";
+    console.log("hello");
+    // alert(Ignoring me?);
+}, 1000);
