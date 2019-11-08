a88bb918d7347f8d6c8da27684221c9c734b54aa
Dashel Bradshaw <Bradshawdc@s.dcsdk12.org>
2019-11-08 09:24:25 -0700

    First commit

---
+++ jspractice/practicejava.js
@@ -0,0 +1,55 @@
+let favs = document.forms[0];
+console.log
+let color = favs[0];
+
+console.log(colorEl.value);
+
+color.onchange = function() {
+    console.log(this.value);
+};
+
+
+
+
+
+
+
+
+
+
+
+// cs.style.background = "Ff0000";
+// cs.style.textAlign = "center";
+cs.classList.add("highlighted");
+// cs.classList.remove("normal");
+
+// cs.innerHTML = "banana";
+
+// let elem = document.createElement('div');
+// elem.innerHTML = "<p>This is new</p>";
+// elem.className = "special interesting"
+// cs.append(elem);
+// cs.prepend(elem);
+
+// let x= 0;
+// let fruits = ["Apple", "Banana", "Watermelon"];
+
+//while (x < fruits.length) {
+//    if (fruits[x] === "Banana") {
+//        list.append("BBBBBBB");
+//    }   else {
+//        list.append(fruits[x])
+//    }
+//    x++;
+//}
+
+// setTimeout(() => {
+//    watermelon.innerHTML = "<p><strong>" + fruit + "</strong></p>";
+//    console.log("Hi");
+//}, 2000);
+
+// let addFruit = function(fruit) {
+//     watermelon.append(fruit);
+// };
+
+// addFruit("strawberry");
